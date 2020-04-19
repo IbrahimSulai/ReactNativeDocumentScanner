f72b08f3ebda6a5864666ebc55a3138022b6cb00
ibrahim.kce@icloud.com <ibrahim.kce@icloud.com>
2020-04-19 20:01:39 +0530

    Updated the view

--- App.js
+++ App.js
@@ -8,20 +8,12 @@
 
 import React from 'react';
 import {
-  SafeAreaView,
   StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
+  View
 } from 'react-native';
 
 import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
+  Colors
 } from 'react-native/Libraries/NewAppScreen';
 
 import DocumentScanner from "@woonivers/react-native-document-scanner";
@@ -32,70 +24,27 @@ const handleOnPictureTaken = () => {
 
 const App: () => React$Node = () => {
   return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <View style={styles.body}>
+      <View style={styles.container}>
           <DocumentScanner
-              style={styles.scanner}
-              onPictureTaken={() => handleOnPictureTaken}
-              overlayColor="rgba(255,130,0, 0.7)"
-              enableTorch={false}
-              quality={0.5}
-              detectionCountBeforeCapture={5}
-              detectionRefreshRateInMS={50}
-            />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
+            style={styles.scanner}
+            onPictureTaken={() => handleOnPictureTaken}
+            overlayColor="rgba(255,130,0, 0.7)"
+            enableTorch={false}
+            quality={0.5}
+            detectionCountBeforeCapture={5}
+            detectionRefreshRateInMS={50}
+          />
+        </View>
   );
 }
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter
+  container: {
+    flex:1
   },
   scanner: {
-    margin: 20,
-    height: 400
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
+    flex:1
+  }
 });
 
 export default App;
